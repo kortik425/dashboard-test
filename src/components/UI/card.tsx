e0f7f93aeb5140b87725d90f39fef88b1c8a9267
kortik425 <jaycee425.develop@gmail.com>
2024-10-22 05:28:57 +0200

    feat: Add card

---
+++ src/components/UI/card.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+interface CardProps {
+  title: string;
+  content?: string;
+  className: string;
+  onClick?: (e: React.MouseEvent) => void;
+}
+
+interface CardContainerProps {
+  children: React.ReactNode;
+  className: string;
+  onClick?: (e: React.MouseEvent) => void;
+}
+
+const Card: React.FC<CardProps> = ({ title, content, className, ...rest }) => {
+  return (
+    <CardContainer
+      className={`p-4 w-full h-[148px] bg-sky-600 rounded-2xl text-left ${className}`}
+      {...rest}
+    >
+      <h3>{title}</h3>
+      {content && <p>{content}</p>}
+    </CardContainer>
+  );
+};
+
+const CardContainer: React.FC<CardContainerProps> = ({
+  children,
+  onClick,
+  className,
+}) => {
+  if (!onClick) {
+    return <article className={className}>{children}</article>;
+  }
+
+  return (
+    <button className={className} onClick={onClick}>
+      <span>{children}</span>
+    </button>
+  );
+};
+
+export default Card;
--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,2 +1,3 @@
 export { default as TextInput } from "./text-input";
 export { default as Tooltip } from "./tooltip";
+export { default as Card } from "./card";
