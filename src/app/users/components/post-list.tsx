7e316f7875a57b860d8070f22f8c5cc5b4d09922
kortik425 <jaycee425.develop@gmail.com>
2024-10-23 02:14:13 +0200

    chore: Add Posts List per user

---
+++ src/app/users/components/post-list.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { BiX } from "react-icons/bi";
+
+import { Card } from "@/components/UI";
+import { useDataContext } from "@/context/data-context";
+
+const PostList: React.FC = () => {
+  const { postList, getPost, setUid } = useDataContext();
+
+  const showPost = (e: React.MouseEvent, postId: number) => {
+    getPost(postId);
+  };
+  const isEmpty = postList.length === 0;
+  console.log(isEmpty);
+  return (
+    <section
+      className={`bg-gray-900 rounded-2xl fixed top-0 bottom-0 max-w-96 m-4 overflow-y-auto transition-all ${isEmpty ? "-right-full" : "right-0"}`}
+    >
+      <header className="flex justify-between m-4">
+        <h2>postList</h2>
+        <button
+          onClick={() => {
+            setUid(null);
+          }}
+        >
+          <BiX size={44} />
+        </button>
+      </header>
+      <div className="m-4">
+        {postList.length !== 0 &&
+          postList.toReversed().map((post) => {
+            return (
+              <Card
+                key={post.id}
+                title={post.title}
+                className="mb-5"
+                onClick={(e) => {
+                  showPost(e, post.id);
+                }}
+              />
+            );
+          })}
+      </div>
+    </section>
+  );
+};
+
+export default PostList;
